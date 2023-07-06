2ad5e32985bad4e8b7f018f4a6edd08b6eb1927a
NaveenprasanthT <naveenprasanth2109@gmail.com>
2023-07-06 11:50:28 +0530

    Testimonials Updated

--- src/Components/Home/Partners.jsx
+++ src/Components/Home/Partners.jsx
@@ -6,40 +6,70 @@ const Partners = () => {
 
     const data = [
         {
-            img: '/Assets/Partners/ap.png',
+            img: '/Assets/Partners/edited/2.png',
         },
         {
-            img: '/Assets/Partners/partner1.png',
+            img: '/Assets/Partners/edited/3.png',
         },
         {
-            img: '/Assets/Partners/partner2.png',
+            img: '/Assets/Partners/edited/4.png',
         },
         {
-            img: '/Assets/Partners/partner3.png',
+            img: '/Assets/Partners/edited/5.png',
         },
         {
-            img: '/Assets/Partners/partner4.png',
+            img: '/Assets/Partners/edited/6.png',
         },
         {
-            img: '/Assets/Partners/partner5.png',
+            img: '/Assets/Partners/edited/7.png',
+        },
+        //temp
+        {
+            img: '/Assets/Partners/edited/1.png',
         },
         {
-            img: '/Assets/Partners/elica.png',
+            img: '/Assets/Partners/edited/8.png',
         },
         {
-            img: '/Assets/Partners/greenply.png',
+            img: '/Assets/Partners/edited/9.png',
+        },
+        //
+        //Double time
+        {
+            img: '/Assets/Partners/edited/2.png',
         },
         {
-            img: '/Assets/Partners/Layer1.png',
+            img: '/Assets/Partners/edited/3.png',
+        },
+        {
+            img: '/Assets/Partners/edited/4.png',
+        },
+        {
+            img: '/Assets/Partners/edited/5.png',
+        },
+        {
+            img: '/Assets/Partners/edited/6.png',
+        },
+        {
+            img: '/Assets/Partners/edited/7.png',
+        },
+        //temp
+        {
+            img: '/Assets/Partners/edited/1.png',
+        },
+        {
+            img: '/Assets/Partners/edited/8.png',
+        },
+        {
+            img: '/Assets/Partners/edited/9.png',
         },
     ]
 
     return (
-        <div className={styles.sectionWrap}>
             <div className={styles.sectionWrap}>
                 <div className={styles.title}>Trusted Partners</div>
                 <p>An elite list of partners who strengthen our brand promise.</p>
-                <div className={styles.cardsWrapper}>
+                <div className={styles.cardsContainer}>
                     <div className={styles.cards}>
                         {
                             data.map((item, key) => (
@@ -49,18 +79,8 @@ const Partners = () => {
                             ))
                         }
                     </div>
-                    {/*<div className={styles.cards}>
-                        {
-                            data.map((item, key) => (
-                                <div className={styles.card} key={key}>
-                                    <Image width={1000} height={1000} className={styles.image} src={item.img} alt='Images' />
-                                </div>
-                            ))
-                        }
-                    </div> */}
                 </div>
             </div>
-        </div>
     )
 }
 
